623dffeef7adfb1ad4f30cd6ba0647ee7b484a03
priit79 <priit.enno@gmail.com>
2023-02-11 14:55:05 +0200

    add teacher table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Student} from "./school/shared/models/Student";
+import {Teacher} from "./school/shared/models/Teacher";
 
 
 
@@ -11,29 +12,38 @@ import {Student} from "./school/shared/models/Student";
 export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.students = this.fetchStoodents();
+    this.teachers = this.fetchTeaachers();
   }
   title = 'angular-student-teacher-exercise';
 
   students: Student[] = [];
   studentNames: string[] = ['Imbi', 'Liisu', 'August'];
 
+  teachers: Teacher[] = [];
+  teacherNames: string[] = ['Vinod', 'Uche', 'Laur'];
+
+
   fetchStoodents(): Student[] {
     let stoodents: Student[] = [];
-
     stoodents.push(new Student('Eedu', '6st grade'));
-
     return stoodents;
+  }
+
+  fetchTeaachers(): Teacher[] {
+    let teaachers: Teacher[] = [];
+    teaachers.push(new Teacher('Udupets', 'mathematics'))
+    return teaachers;
   }
 
   addStudent() {
     let studentName = this.studentNames.at(Math.floor(Math.random() * this.studentNames.length));
     // @ts-ignore
     this.students.push(new Student(studentName, '10th grade'))
-
-
   }
 
   addTeacher() {
-
+    let teacherName = this.teacherNames.at(Math.floor(Math.random() * this.teacherNames.length));
+    // @ts-ignore
+    this.teachers.push(new Teacher(teacherName, 'programming'))
   }
 }
